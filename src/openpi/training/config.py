040cfed77baf26ce7a74c11980dadcfc212f28b1
jungwonshin <jung@deftai.co>
2026-03-10 17:07:12 -0700

    changed default trainconfig save interval and keep period

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -644,9 +644,9 @@ class TrainConfig:
     # How often (in steps) to log training metrics.
     log_interval: int = 100
     # How often (in steps) to save checkpoints.
-    save_interval: int = 1000
+    save_interval: int = 5000
     # If set, any existing checkpoints matching step % keep_period == 0 will not be deleted.
-    keep_period: int | None = 5000
+    keep_period: int | None = 10000
 
     # If true, will overwrite the checkpoint directory if it already exists.
     overwrite: bool = False
